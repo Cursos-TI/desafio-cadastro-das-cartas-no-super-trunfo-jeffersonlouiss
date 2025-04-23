c16ed5fede81db0c72030c7c928a82b25fe430c5
JeffersonLouis <42342922+jeffersonlouiss@users.noreply.github.com>
2025-04-23 00:10:08 +0000

    Implementado nivel aventureiro do superTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,8 @@ int main()
     float area_0, area_1;
     float pib_0, pib_1;
     int nPontosTuris_0, nPontosTuris_1;
+    float densidadePopulacional_0, densidadePopulacional_1;
+    float pibPerCapita_0, pibPerCapita_1;
 
     // Cadastro das Cartas 01: entrada pelo usuario
     printf("---CADASTRO DA CARTA N-01---\n");
@@ -34,6 +36,10 @@ int main()
     printf("Número de Pontos Turísticos: ");
     scanf("%i", &nPontosTuris_0);
 
+    //calcula os atributos desidade populacional e PIB per Capita da carta 1
+    densidadePopulacional_0 = populacao_0 / area_0;
+    pibPerCapita_0 =  pib_0 / populacao_0;
+
     // Cadastro das Cartas 02: entrada pelo usuario
     printf("---CADASTRO DA CARTA N-02---\n");
     printf("Estado: ");
@@ -51,6 +57,10 @@ int main()
     printf("Número de Pontos Turísticos: ");
     scanf("%i", &nPontosTuris_1);
 
+    //calcula os atributos desidade populacional e PIB per Capita da carta 2
+    densidadePopulacional_1 = populacao_1 / area_1;
+    pibPerCapita_1 =  pib_1 / populacao_1;
+
     printf("\n\n");
     // Exibicao dos dados de cada carta numero 1
     printf("Carta 1:\n");
@@ -58,9 +68,11 @@ int main()
     printf("Código: %s\n", cod_0);
     printf("Nome da Cidade: %s\n", cidade_0);
     printf("População: %i\n", populacao_0);
-    printf("Área: %f km²\n", area_0);
-    printf("PIB: %f bilhões de reais\n", pib_0);
+    printf("Área: %0.2f km²\n", area_0);
+    printf("PIB: %0.2f bilhões de reais\n", pib_0);
     printf("Número de Pontos Turísticos: %i\n", nPontosTuris_0);
+    printf("Densidade Populacional: %0.2f\n", densidadePopulacional_0);
+    printf("PIB per Capita: %0.2f\n", pibPerCapita_0);
 
     printf("\n\n");
     // Exibicao dos dados de cada carta numero 2
@@ -69,9 +81,11 @@ int main()
     printf("Código: %s\n", cod_1);
     printf("Nome da Cidade: %s\n", cidade_1);
     printf("População: %i\n", populacao_1);
-    printf("Área: %f km²\n", area_1);
-    printf("PIB: %f bilhões de reais\n", pib_1);
+    printf("Área: %0.2f km²\n", area_1);
+    printf("PIB: %0.2f bilhões de reais\n", pib_1);
     printf("Número de Pontos Turísticos: %i\n", nPontosTuris_1);
+    printf("Densidade Populacional: %0.2f\n", densidadePopulacional_1);
+    printf("PIB per Capita: %0.2f\n", pibPerCapita_1);
 
     return 0;
 }
